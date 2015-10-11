f48427ff5f371c3ff13ef9cd4f8f25b247184774
k3k3r <glebun1@mail.ru>
2015-10-11 20:15:37 +0800

    da

--- TestSystem/TestSystem/Program.cs
+++ TestSystem/TestSystem/Program.cs
@@ -14,6 +14,7 @@ namespace TestSystem
         [STAThread]
         static void Main()
         {
+            Console.WriteLine("Hello Yarolav");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
