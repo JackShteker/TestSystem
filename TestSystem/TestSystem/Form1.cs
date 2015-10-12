83e29b1ee909760d3db877467b04f56f39e9d1f4
k3k3r <glebun1@mail.ru>
2015-10-13 02:28:10 +0800

    312

--- TestSystem/TestSystem/Form1.cs
+++ TestSystem/TestSystem/Form1.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace TestSystem
 {
     public partial class Form1 : Form
@@ -22,6 +14,47 @@ namespace TestSystem
             MessageBox.Show("Hello Yarolav");
             TaskList taskList = new TaskList();
             MessageBox.Show(taskList.GetTask(1).GetQuestion());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tableLayoutPanel1.Width = this.Width;
+            tableLayoutPanel1.Height = this.Height;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
