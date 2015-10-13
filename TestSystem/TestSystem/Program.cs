1cf18fe9f224de7ea5f1a8308cf9c237e08cd854
k3k3r <glebun1@mail.ru>
2015-10-13 22:59:00 +0800

    131231

--- TestSystem/TestSystem/Program.cs
+++ TestSystem/TestSystem/Program.cs
@@ -17,6 +17,7 @@ namespace TestSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
