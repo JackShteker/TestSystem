7db3458c6ecd0f7a69fb06052afcdd920985e095
Yaric Lol <yroslav541@gmail.com>
2015-10-13 18:31:51 +0300

    suka loh

--- TestSystem/TestSystem/Form1.cs
+++ TestSystem/TestSystem/Form1.cs
@@ -5,6 +5,8 @@ namespace TestSystem
     public partial class Form1 : Form
     {
         TaskList taskList = new TaskList();
+        bool [] correctAnswers = new bool [taskList.len()];
+        byte index;
         public Form1()
         {
             InitializeComponent();
@@ -25,7 +27,41 @@ namespace TestSystem
         }
         private void button1_Click(object sender, EventArgs e)
         {
-
+            if (radioAnswer1.checked)
+            {
+                if (taskList[index].SendAnswer(radioAnswer1.Text))
+                {
+                    correctAnswers[index] = true;
+                }
+            }
+            else if (radioAnswer2.checked)
+            {
+                if (taskList[index].SendAnswer(radioAnswer2.Text))
+                {
+                    correctAnswers[index] = true;
+                }
+            }
+            else if (radioAnswer3.checked)
+            {
+                if (taskList[index].SendAnswer(radioAnswer3.Text))
+                {
+                    correctAnswers[index] = true;
+                }        
+            }
+            else if (radioAnswer4.checked)
+            {
+                if (taskList[index].SendAnswer(radioAnswer4.Text))
+                {
+                    correctAnswers[index] = true;
+                }         
+            }
+            else if (radioAnswer5.checked)
+            {
+                if (taskList[index].SendAnswer(radioAnswer5.Text))
+                {
+                    correctAnswers[index] = true;
+                }
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -33,6 +69,10 @@ namespace TestSystem
             label1.Text = taskList.GetTask(1).GetQuestion();
             string[] answers = taskList.GetTask(1).GetAnswers();
             radioAnswer1.Text = answers[0];
+            radioAnswer2.Text = answers[1];
+            radioAnswer3.Text = answers[2];
+            radioAnswer4.Text = answers[3];
+            radioAnswer5.Text = answers[4];
         }
     }
 }
