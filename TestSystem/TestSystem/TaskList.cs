ff2b98e7b28e23fecacd8ee24909013d9c61a960
k3k3r <glebun1@mail.ru>
2015-10-20 03:46:06 +0800

    1313

--- TestSystem/TestSystem/TaskList.cs
+++ TestSystem/TestSystem/TaskList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Windows.Forms;
 
 namespace TestSystem
 {   
@@ -8,12 +9,12 @@ namespace TestSystem
     {
        public  class Task
         {
-            const byte maxCode = 14;
+            ///const byte maxCode = 14;
             private string Question;
             private string [] Ans = new string[7];
-            private string[] codeSample = new string[maxCode];
+            private string[] codeSample = new string[14];
             private string Correct;
-            public Task(string Question, string[] codeSample, string[] Answers, string correct)
+            public Task(string Question, string[] codeSample, int maxCode, string[] Answers, string correct)
             {
                 this.Question = Question;
                 Array.Copy(codeSample, this.codeSample , maxCode);
@@ -45,26 +46,30 @@ namespace TestSystem
         private int Length;
         public TaskList()
         {
-            const byte maxCode = 14;
+            ///const byte maxCode = 14;
             // MessageBox.Show(Directory.GetCurrentDirectory());
             using (StreamReader readStream = new StreamReader(Directory.GetCurrentDirectory()+"\\Tasks.txt"))
             {
                 Length = Convert.ToInt16(readStream.ReadLine());
                 int current = 0;
                 string[] lines = new string[7];
-                string[] code = new string[maxCode];
+                string[] code = new string[14];
                 string Quest;
                 string correct = "";
+                string kod;
                // while (readStream.Peek() != -1)
                  for(int p=0; p<Length; p++)
                 {
                     Quest = readStream.ReadLine();
                     int j = 0;
-                    code[j] = readStream.ReadLine();
-                    j++;
-                    while (code[j-1][0] != '&')
-                    { 
-                        code[j] = readStream.ReadLine();
+                    int maxCode = 0;
+                    kod = readStream.ReadLine();
+                    ///MessageBox.Show(kod);
+                    while (kod[0] != '&')
+                    {
+                        maxCode++;
+                        code[j] = kod;
+                        kod = readStream.ReadLine();
                         j++;                                    
                     }
                     for (int i = 0; i < 7; i++)
@@ -78,7 +83,7 @@ namespace TestSystem
                         }
                     }
                     //MessageBox.Show(lines[0]+"current="+current.ToString());
-                    Tasks[current] = new Task(Quest, code, lines, correct);
+                    Tasks[current] = new Task(Quest, code, maxCode, lines, correct);
                     current++;
                     //MessageBox.Show(Tasks[current-1].GetAnswers()[0]+ Tasks[current - 1].GetAnswers()[1] + Tasks[current - 1].GetAnswers()[2] + Tasks[current - 1].GetAnswers()[3] + Tasks[current - 1].GetAnswers()[4]);
                    // MessageBox.Show("current="+(current-1).ToString()+Tasks[current-1].GetAnswers()[0]);
